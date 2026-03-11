26959d21fe8e5b2edeed2b58fd6de0ce24e8a833
sadah00 <kimberlysada30@gmail.com>
2026-03-11 23:48:21 +0300

    Refactor ProductsTable component for improved styling and structure

--- latest/src/components/ProductsTable.jsx
+++ latest/src/components/ProductsTable.jsx
@@ -2,27 +2,26 @@ import React from 'react'
 
 const ProductsTable = () => {
   return (
-    <div>
-      <div>
-          <table>
-              <thead>
-                  <tr>
-                    <th>Prod ID</th>
-                    <th>Name</th>
-                    <th>Buying</th>
-                    <th>Selling price</th>
-                  </tr>
-              </thead>
-              <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Milk Cream</td>
-                    <td>360</td>
-                    <td>540</td>
-                </tr>
-              </tbody>
-          </table>
-      </div>
+    <div className="p-4">
+      <table className="table-auto border-collapse border border-gray-400 w-100">
+        <thead className="bg-gray-200">
+          <tr>
+            <th className="border border-gray-400 px-4 py-2">Prod ID</th>
+            <th className="border border-gray-400 px-4 py-2">Name</th>
+            <th className="border border-gray-400 px-4 py-2">Buying</th>
+            <th className="border border-gray-400 px-4 py-2">Selling Price</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr className="text-center">
+            <td className="border border-gray-400 px-4 py-2">1</td>
+            <td className="border border-gray-400 px-4 py-2">Milk Cream</td>
+            <td className="border border-gray-400 px-4 py-2">360</td>
+            <td className="border border-gray-400 px-4 py-2">540</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
